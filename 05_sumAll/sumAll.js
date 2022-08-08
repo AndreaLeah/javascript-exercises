f4665b60e84535a09a77e0722743624034acc689
Angelia <thompson27thompson@gmail.com>
2022-08-08 18:13:03 -0400

    Complete Test 1

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sumzy = 0;
+    for (let i = num1; i >= num1 && i <= num2; i++) {
+        sumzy += i;
+    }
+    return sumzy;
 };
 
 // Do not edit below this line
