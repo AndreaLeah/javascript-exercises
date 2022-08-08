60e57fa6067ca3049b0ed73e281253c54f0bd5f6
Angelia <thompson27thompson@gmail.com>
2022-08-08 19:38:37 -0400

    Test 5 Complete: Return Error For Non-Number Parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,16 +1,16 @@
 const sumAll = function(num1, num2) {
     let sumzy = 0;
-    if (num1 <0 || num2 < 0) {
+    if (num1 <0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number") {
         return "ERROR";
     } 
     else {
         if (num1 < num2) {
             for (let i = num1; i >= num1 && i <= num2; i++) {
-            sumzy += i;
+                sumzy += i;
         }} 
         else {
             for (let i = num1; i <= num1 && i >= num2; i--) {
-            sumzy += i;
+                sumzy += i;
         }}
     return sumzy;
         }
