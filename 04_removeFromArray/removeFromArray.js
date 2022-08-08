186842f6c242dc4e1fda7913be848f43021936de
Angelia <thompson27thompson@gmail.com>
2022-08-08 18:01:43 -0400

    Complete Test 7: Strict Equality Added

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
 const removeFromArray = function(arr, ...nums) {
     for (let j =0; j < nums.length; j++) {
         for (let i = 0; i < arr.length; i++) {
-            if (arr[i] == nums[j]) {
+            if (arr[i] === nums[j]) {
                 arr.splice(i, 1);
             }
         }
