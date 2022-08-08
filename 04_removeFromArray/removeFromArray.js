0b0480b73a06c78e7e7ebe6d8518d5835e3f79be
Angelia <thompson27thompson@gmail.com>
2022-08-08 17:52:53 -0400

    Complete Test 2: Accept Undefined Amount of Multiple Args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,12 @@
-const removeFromArray = function(arr, num) {
-    for (let i = 0; i < arr.length; i ++)
-        if (arr[i] == num) {
-            arr.splice(i, 1);
-            return arr;
+const removeFromArray = function(arr, ...nums) {
+    for (let j =0; j < nums.length; j++) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] == nums[j]) {
+                arr.splice(i, 1);
+            }
         }
+    }
+        return arr;
 };
 
 // Do not edit below this line
