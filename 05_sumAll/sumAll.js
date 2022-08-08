91f2801d9ea5480e57637fb273e7666de533ff88
Angelia <thompson27thompson@gmail.com>
2022-08-08 18:27:40 -0400

    Complete Test 4: Return Error With Negative Numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,19 @@
 const sumAll = function(num1, num2) {
     let sumzy = 0;
-    if (num1 < num2) {
-    for (let i = num1; i >= num1 && i <= num2; i++) {
-        sumzy += i;
-    }
-} else {
-    for (let i = num1; i <= num1 && i >= num2; i--) {
-        sumzy += i;
-    }
-}
-return sumzy;
+    if (num1 <0 || num2 < 0) {
+        return "ERROR";
+    } 
+    else {
+        if (num1 < num2) {
+            for (let i = num1; i >= num1 && i <= num2; i++) {
+            sumzy += i;
+        }} 
+        else {
+            for (let i = num1; i <= num1 && i >= num2; i--) {
+            sumzy += i;
+        }}
+    return sumzy;
+        }
 };
 
 // Do not edit below this line
