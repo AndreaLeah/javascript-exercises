8f0a9d3b5262dd4706faed0ed52d78c874965033
Angelia <thompson27thompson@gmail.com>
2022-08-08 18:23:08 -0400

    Complete Test 3: Can Accept Larger Number First

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,9 +1,15 @@
 const sumAll = function(num1, num2) {
     let sumzy = 0;
+    if (num1 < num2) {
     for (let i = num1; i >= num1 && i <= num2; i++) {
         sumzy += i;
     }
-    return sumzy;
+} else {
+    for (let i = num1; i <= num1 && i >= num2; i--) {
+        sumzy += i;
+    }
+}
+return sumzy;
 };
 
 // Do not edit below this line
