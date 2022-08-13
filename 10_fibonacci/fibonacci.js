b81032ebf58c463768e2ae9fdce7ae9ac78f19a2
Angelia <thompson27thompson@gmail.com>
2022-08-13 02:34:58 -0400

    All Tests Complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let arr = [1, 1];
+    if (n < 0) {
+        return "OOPS";
+    }
+    else if (n == 1 || n ==2) {
+        return 1
+    }
+    else {
+        for (let i = 0, j = 1; (j + 1) < n; i++, j++) {
+            let newValue = arr[i] + arr[j];
+            arr.push(newValue);
+        }
+    }
+    return arr[arr.length - 1];
 };
 
 // Do not edit below this line
