621b001b071bd911dd1b16fedacbc56a18058417
Angelia <thompson27thompson@gmail.com>
2022-08-13 01:58:41 -0400

    Palindromes Tests Complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (arg) {
+    const result = arg.replace(/[!"#$%&'()*+,-./:;<=>\s?@[\]^_`{|}~]/g, '').toLowerCase();
+    let reversed = result.split('').reverse().join('');
+    return result === reversed;
 };
 
 // Do not edit below this line
