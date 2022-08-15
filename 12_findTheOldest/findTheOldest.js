0129f1ae69062b9cff0b4c15539b8963502f37c3
Angelia <thompson27thompson@gmail.com>
2022-08-14 23:57:46 -0400

    Test 2 Complete: Account For Still Living

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = 0;
+    let name = 'Placeholder';
+    for (let i = 0; i < people.length; i ++) {
+        let ageInYears = people[i].yearOfDeath - people[i].yearOfBirth;
+        let personName = people[i];
+         if (isNaN(ageInYears) == true) {
+           ageInYears = 2022-people[i].yearOfBirth;
+         }
+         if (oldest == 0) {
+            oldest = ageInYears;
+            name = personName;
+         }
+        else {
+            if (ageInYears > oldest) {
+                oldest = ageInYears;
+                name = personName;
+            }
+            else {
+                continue
+            }}}
+    return name;
 };
 
 // Do not edit below this line
