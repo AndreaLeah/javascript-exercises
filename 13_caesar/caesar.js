3ac2b2f2c9eb13ecef538c5894a07af168ae10ba
Angelia <thompson27thompson@gmail.com>
2022-08-15 22:09:13 -0400

    Caesar Cipher Function Encodes Phrases with Punctuation

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,20 +1,38 @@
 const caesar = function(string, num) {
     let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
+  
     string = string.split('');
     let newString = "";
     for (let letter of string) {
-        if (letter == letter.toUpperCase()) {
-            let i = upperCase.indexOf(letter);
-            let uLetter = upperCase[i + num];
-            newString += uLetter;
+        if (/[a-zA-Z]/.test(letter) === false) {
+            newString += letter;
         }
         else {
-            let i = lowerCase.indexOf(letter);
-            let lLetter = lowerCase[i + num];
-            newString += lLetter;
-        }}
+            if (letter == letter.toUpperCase()) {
+                let uLetter;
+                let i = upperCase.indexOf(letter);
+                if (i + num > 25) {
+                  i = ((i + num) - 1) - 25
+                  uLetter = upperCase[i];
+                }
+                else {
+                uLetter = upperCase[i + num];
+                }
+                newString += uLetter;
+            }
+            else {
+                let lLetter;
+                let i = lowerCase.indexOf(letter);
+                if (i + num > 25) {
+                  i = ((i + num) - 1) - 25
+                  lLetter = upperCase[i];
+                }
+                else {
+                lLetter = lowerCase[i + num];
+                }
+                newString += lLetter;
+            }}}
     return newString;
     };
 
